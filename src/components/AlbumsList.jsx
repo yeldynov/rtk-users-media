3963b78f401efc8343a3412416e99d9411918333
Nico Yeldynov <yeldynov@gmail.com>
2023-04-08 19:55:52 +0300

    Getting started with rtk query. Added first query for fetching albums for a user

--- src/components/AlbumsList.jsx
+++ src/components/AlbumsList.jsx
@@ -1,7 +1,34 @@
-import React from 'react';
+import { useFetchAlbumsQuery } from '../store';
+import Skeleton from './Skeleton';
+import ExpandablePanel from './ExpandablePanel';
+import Button from './Button';
 
 function AlbumsList({ user }) {
-  return <div>Albums for {user.name}</div>;
+  const { data, error, isLoading } = useFetchAlbumsQuery(user);
+
+  let content;
+  if (isLoading) {
+    content = <Skeleton times={3} />;
+  } else if (error) {
+    content = <div>Error loading albums.</div>;
+  } else {
+    content = data.map((album) => {
+      const header = <div>{album.title}</div>;
+
+      return (
+        <ExpandablePanel key={album.id} header={header}>
+          List of photos in the album
+        </ExpandablePanel>
+      );
+    });
+  }
+
+  return (
+    <div>
+      <div>Albums for {user.name}</div>
+      <div>{content}</div>
+    </div>
+  );
 }
 
 export default AlbumsList;
