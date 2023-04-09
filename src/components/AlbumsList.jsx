d5f8d69d25a39dd79c2647add7e2413dbed8adb6
Nico Yeldynov <yeldynov@gmail.com>
2023-04-09 14:41:34 +0300

    implemented adding album feature, as well as updating store on add using tags system

--- src/components/AlbumsList.jsx
+++ src/components/AlbumsList.jsx
@@ -1,14 +1,19 @@
-import { useFetchAlbumsQuery } from '../store';
+import { useFetchAlbumsQuery, useAddAlbumMutation } from '../store';
 import Skeleton from './Skeleton';
 import ExpandablePanel from './ExpandablePanel';
 import Button from './Button';
 
 function AlbumsList({ user }) {
   const { data, error, isLoading } = useFetchAlbumsQuery(user);
+  const [addAlbum, results] = useAddAlbumMutation();
+
+  const handleAddAlbum = () => {
+    addAlbum(user);
+  };
 
   let content;
   if (isLoading) {
-    content = <Skeleton times={3} />;
+    content = <Skeleton className='h-10 w-full' times={3} />;
   } else if (error) {
     content = <div>Error loading albums.</div>;
   } else {
@@ -25,7 +30,12 @@ function AlbumsList({ user }) {
 
   return (
     <div>
-      <div>Albums for {user.name}</div>
+      <div className='m-2 flex flex-row items-center justify-between'>
+        <h3 className='text-lg font-bold'>Albums for {user.name}</h3>
+        <Button loading={results.isLoading} onClick={handleAddAlbum}>
+          + Add Album
+        </Button>
+      </div>
       <div>{content}</div>
     </div>
   );
